33941183ba35ab7ee2fd6559ae67cad1bf9485a2
Ravi Vooda <rvooda@uber.com>
2020-10-20 13:38:14 -0700

    Finding reference element (return hardcoded)

--- src/main/java/org/intellij/sdk/codeInspection/ForChecker.java
+++ src/main/java/org/intellij/sdk/codeInspection/ForChecker.java
@@ -2,18 +2,19 @@ package org.intellij.sdk.codeInspection;
 
 import com.goide.inspections.core.GoInspectionBase;
 import com.goide.inspections.core.GoProblemsHolder;
-import com.goide.psi.GoForStatement;
-import com.goide.psi.GoVisitor;
+import com.goide.psi.*;
 import com.intellij.codeInspection.LocalInspectionToolSession;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class ForChecker extends GoInspectionBase {
@@ -23,30 +24,94 @@ public class ForChecker extends GoInspectionBase {
     protected @NotNull GoVisitor buildGoVisitor(
             @NotNull GoProblemsHolder holder,
             @NotNull LocalInspectionToolSession session) {
-        LOG.warn("Ravi: Got inside the buildGoVisitor");
         return new GoVisitor() {
             @Override
-            public void visitForStatement(@NotNull GoForStatement o) {
-                LOG.warn("Ravi: Got inside the visitForStatement: " + o.getFor().getText());
-                PsiElement assignedVariable = getFaultyForLoop(o);
+            public void visitCallExpr(@NotNull GoCallExpr o) {
+                super.visitCallExpr(o);
+
+                GoForStatement forStatement = PsiTreeUtil.getParentOfType(o, GoForStatement.class);
+                if (forStatement == null) return;
+
+                PsiElement assignedVariable = getAssignedVariable(forStatement);
                 if (assignedVariable == null) return;
 
-                logChildren(o);
+                ArrayList<PsiElement> usages = getUsages(forStatement, assignedVariable);
 
-                ArrayList<PsiElement> blockUsages = getUsages(o.getBlock(), assignedVariable);
-                if (blockUsages.size() == 0) {
-                    LOG.warn("Ravi: Did not find any usages of the assignedVariable: " + assignedVariable.getText());
-                    return;
+                LOG.warn("\n\n\n");
+
+                LOG.warn("visitCallExpr: " + o + ", text: " + o.getText() + "\nargument list: " + o.getArgumentList().getText() + "\nexpression: " + o.getExpression().getText());
+
+                PsiReference psiReference = o.getExpression().getReference();
+                PsiElement resolved = psiReference != null ? psiReference.resolve() : null;
+                if (!(resolved instanceof GoFunctionOrMethodDeclaration)) return;
+
+                LOG.warn("visitCallExpr: successfully resolved to " + resolved);
+
+                List<GoExpression> expressionList = o.getArgumentList().getExpressionList();
+                for (GoExpression goExpression : expressionList) {
+                    if (goExpression.isConstant()) continue;
+
+                    LOG.warn("visitCallExpr: expression: " + goExpression + ", text: " + goExpression.getText());
+
+                    if (goExpression instanceof GoUnaryExpr) {
+                        GoUnaryExpr unaryExpr = (GoUnaryExpr) goExpression;
+                        PsiElement operator = unaryExpr.getOperator();
+                        if (operator == null || !operator.getText().equalsIgnoreCase("&")) continue;
+
+                        ArrayList<PsiElement> referenceElements = getReferenceElements(goExpression);
+                        for (PsiElement referenceElement : referenceElements) {
+                            for (PsiElement usage : usages) {
+                                LOG.warn("visitCallExpr: comparing with usages: " + usages.stream().map(PsiElement::getText).collect(Collectors.joining()));
+                                LOG.warn("USAGE reference: " + assignedVariable.getReference());
+                                LOG.warn("REFERENCE reference: " + referenceElement.getReference());
+                                holder.registerProblem(referenceElement, () -> "Passing reference of copied range element");
+                                holder.registerProblem(assignedVariable, () -> "Passing reference of copied range element");
+                                return;
+                                /*if (forStatement.getManager().areElementsEquivalent(referenceElement, usage)) {
+                                    LOG.warn("visitCallExpr: RAVI MATCHED: usage: " + usage.getText() + ", referenceElement: " + referenceElement.getText());
+                                } else {
+                                    LOG.warn("visitCallExpr: did not match usage: " +
+                                            assignedVariable.getText() +
+                                            ", referenceElement: " + referenceElement.getText());
+                                }*/
+                            }
+                        }
+                    }
                 }
-
-                LOG.warn("Ravi: registered a problem for " + assignedVariable.getText() + " for all usages " + blockUsages.stream().map(PsiElement::getText).collect(Collectors.joining()));
-                for (PsiElement usage : blockUsages) {
-                    holder.registerProblem(usage, () -> "Ravi Problem");
-                }
-                holder.registerProblem(assignedVariable, () -> "Ravi Problem");
-
             }
         };
+    }
+
+    @Nullable
+    private PsiElement getDeclarationElement(PsiElement element) {
+        if (element == null) return null;
+
+        if (element.getReference() != null) {
+            element = element.getReference().resolve();
+        }
+
+        return element;
+    }
+
+    private @NotNull ArrayList<PsiElement> getReferenceElements(@NotNull GoExpression expr) {
+        ArrayList<PsiElement> references = new ArrayList<>();
+        if (expr instanceof GoUnaryExpr) {
+            LOG.warn("getSingleReferences unary: " + expr.getText());
+        } else if (expr instanceof GoReferenceExpression) {
+            PsiElement identifier = ((GoReferenceExpression) expr).getIdentifier();
+            LOG.warn("getSingleReferences reference identifier: " + identifier.getText());
+            references.add(identifier);
+        } else {
+            throw new UnsupportedOperationException("unsupported reference type " + expr.getClass().getName());
+        }
+
+        @NotNull PsiElement[] children = expr.getChildren();
+        for (PsiElement child : children) {
+            if (child instanceof GoExpression) {
+                references.addAll(getReferenceElements((GoExpression) child));
+            }
+        }
+        return references;
     }
 
     private @NotNull ArrayList<PsiElement> getUsages(@Nullable PsiElement o, @NotNull PsiElement element) {
@@ -56,20 +121,10 @@ public class ForChecker extends GoInspectionBase {
             return usageElements;
         }
 
-        if (PsiManager.getInstance(o.getProject()).areElementsEquivalent(o, element)) {
-            LOG.warn("Ravi: areElementsEquivalent");
-            ReferencesSearch.search(element, o.getUseScope()).forEach(psiReference -> {
-                ProgressManager.checkCanceled();
-
-            });
-            usageElements.add(o);
-        }
-
-        for (PsiElement child : o.getChildren()) {
-            usageElements.addAll(getUsages(child, element));
-        }
-
-        logChildren(o);
+        ReferencesSearch.search(element, o.getUseScope()).forEach(psiReference -> {
+            ProgressManager.checkCanceled();
+            usageElements.add(psiReference.getElement());
+        });
 
         return usageElements;
     }
@@ -81,31 +136,56 @@ public class ForChecker extends GoInspectionBase {
     }
 
     @Nullable
-    private PsiElement getFaultyForLoop(@NotNull GoForStatement o) {
+    private PsiElement getAssignedVariable(@NotNull GoForStatement o) {
         if (o.getRangeClause() == null) {
-//            LOG.warn("Ravi: did not find any range element");
             return null;
         }
         PsiElement assignedVariable;
         if (o.getRangeClause().getVarAssign() != null) {
-//            LOG.warn("Ravi: " + o.getRangeClause().getVarAssign());
             if (o.getRangeClause().getVarDefinitionList().size() <= 1) {
-//                LOG.warn("Ravi: only using single assignment like a great person in new assignment");
                 return null;
             }
-//            LOG.warn("Ravi: " + o.getRangeClause().getVarDefinitionList().get(0).getName());
-//            LOG.warn("Ravi: " + o.getRangeClause().getVarDefinitionList().get(1).getName());
             assignedVariable = o.getRangeClause().getVarDefinitionList().get(1);
         } else {
             if (o.getRangeClause().getLeftExpressionsList().size() <= 1) {
-//                LOG.warn("Ravi: only using single assignment like a great person for old assignment");
                 return null;
             }
-//            LOG.warn("Ravi: " + o.getRangeClause().getLeftExpressionsList().get(0).getText());
-//            LOG.warn("Ravi: " + o.getRangeClause().getLeftExpressionsList().get(1).getText());
-//            LOG.warn("Ravi: " + o.getRangeClause().getRightExpressionsList().get(0).getText());
             assignedVariable = o.getRangeClause().getLeftExpressionsList().get(1);
         }
         return assignedVariable;
     }
 }
+
+
+/*@Override
+            public void visitForStatement(@NotNull GoForStatement o) {
+                super.visitForStatement(o);
+                if (o.getBlock() == null) {
+                    LOG.warn("Ravi: Empty block");
+                    return;
+                }
+                LOG.warn("Ravi: Got inside the visitForStatement: " + o.getFor().getText());
+                PsiElement assignedVariable = getFaultyForLoop(o);
+                if (assignedVariable == null) return;
+
+                if (assignedVariable.getReference() != null) {
+                    assignedVariable = assignedVariable.getReference().resolve();
+                }
+
+                logChildren(o);
+
+                assert assignedVariable != null;
+                Collection<PsiReference> blockUsages = ReferencesSearch.search(assignedVariable, o.getBlock().getUseScope()).findAll();
+
+                if (blockUsages.size() == 0) {
+                    LOG.warn("Ravi: Did not find any usages of the assignedVariable: " + assignedVariable.getText());
+                    return;
+                }
+
+                LOG.warn("Ravi: registered a problem for " + assignedVariable.getText() + " for all usages " + blockUsages.stream().map(PsiReference::getCanonicalText).collect(Collectors.joining()));
+                *//*for (PsiReference usage : blockUsages) {
+                    holder.registerProblem(usage.getElement(), () -> "Ravi Problem");
+                }
+                holder.registerProblem(assignedVariable, () -> "Ravi Problem");*//*
+
+            }*/
